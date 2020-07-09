2110f7308ef095c5d527d863c4da7eb435052b77
Hiromichi IIZUKA <iizuka@eduam.co.jp>
2020-07-09 09:44:59 +0900

    Add skelton

---
+++ demo/user/main.cpp
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2018 Yamana Laboratory, Waseda University
+ * Supported by JST CREST Grant Number JPMJCR1503, Japan.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE‐2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <unistd.h>
+#include <string>
+#include <iostream>
+#include <stdsc/stdsc_log.hpp>
+#include <stdsc/stdsc_exception.hpp>
+#include <share/define.hpp>
+
+struct Option
+{
+    uint64_t dummy;
+};
+
+void init(Option& option, int argc, char* argv[])
+{
+}
+
+void exec(Option& option)
+{
+}
+
+int main(int argc, char* argv[])
+{
+    STDSC_INIT_LOG();
+    try
+    {
+        Option option;
+        init(option, argc, argv);
+        STDSC_LOG_INFO("Launched User demo app.");
+        exec(option);
+    }
+    catch (stdsc::AbstractException& e)
+    {
+        STDSC_LOG_ERR("Err: %s", e.what());
+    }
+    catch (...)
+    {
+        STDSC_LOG_ERR("Catch unknown exception");
+    }
+
+    return 0;
+}
