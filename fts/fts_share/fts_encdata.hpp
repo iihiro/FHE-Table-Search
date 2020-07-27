7a499cb24c82918a2a827d2c6a58d5a154de6654
Hiromichi Iizuka <iizuka@eduam.co.jp>
2020-07-27 13:21:42 +0900

    Add comments

--- fts/fts_share/fts_encdata.hpp
+++ fts/fts_share/fts_encdata.hpp
@@ -31,20 +31,66 @@ namespace fts_share
  */
 struct EncData : public fts_share::BasicData<seal::Ciphertext>
 {
+    /**
+     * Constructor
+     * @param[in] params encryption parameters
+     */
     explicit EncData(const seal::EncryptionParameters& params);
+
+    /**
+     * Constructor
+     * @param[in] params encryption parameters
+     * @param[in] ctxt ciphertext
+     */
     EncData(const seal::EncryptionParameters& params, const seal::Ciphertext& ctxt);
+
+    /**
+     * Constructor
+     * @param[in] params encryption parameters
+     * @param[in] ctxts ciphertexts
+     */
     EncData(const seal::EncryptionParameters& params, const std::vector<seal::Ciphertext>& ctxts);
     virtual ~EncData(void) = default;
 
+    /**
+     * Encrypt value
+     * @param[in] input_value input value
+     * @param[in] pubkey public key
+     * @param[in] galoiskey galois keys
+     */
     void encrypt(const int64_t input_value,
                  const seal::PublicKey& pubkey,
                  const seal::GaloisKeys& galoiskey);
+
+    /**
+     * Decrypt value
+     * @param[in] seckey security key
+     * @param[in] output_values output values
+     */
     void decrypt(const seal::SecretKey& seckey, std::vector<int64_t>& output_values) const;
 
+    /**
+     * Save ciphertexts to stream
+     * @param[out] os output stream
+     */
     virtual void save_to_stream(std::ostream& os) const override;
+
+    /**
+     * Load ciphertexts from stream
+     * @param[in] is input stream
+     */
     virtual void load_from_stream(std::istream& is) override;
 
+    /**
+     * Save ciphertexts to file
+     * @param[in] filepath filepath
+     */
     void save_to_file(const std::string& filepath) const;
+
+    /**
+     * Load ciphertexts from file
+     * @param[in] filepath filepath
+     */
     void load_from_file(const std::string& filepath);
 
 private:
