5970abd27dc85314ddfde9c85d04139f0f8638eb
Hiromichi Iizuka <iizuka@eduam.co.jp>
2020-07-27 15:47:59 +0900

    Add tools to make sample table

---
+++ tools/make_table/one-input/make_table.cpp
@@ -0,0 +1,50 @@
+//This code makes a demo table, not for real function
+#include <iostream>
+#include <cassert>
+#include <cstdio>
+#include <fstream>
+#include <chrono>
+#include <random>
+#include <vector>
+#include <thread>
+#include <future>
+#include <chrono>
+
+//number of columns is a half of slot count!
+#define TABLE_SIZE_COL 819200
+#define TABLE_SIZE_ROW 2
+
+int main(void)
+{
+    std::vector<std::vector<long>> inputTable;
+    std::vector<long> inputTable_row;
+
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::mt19937 generator(seed);
+
+    for(int i=0 ; i<TABLE_SIZE_ROW ; i++){
+        for(int j=0 ; j<TABLE_SIZE_COL ; j++){
+            if (j < 36) {
+                inputTable_row.push_back(j+1);
+            } else {
+                inputTable_row.push_back(100);
+            }
+        }
+        inputTable.push_back(inputTable_row);
+        inputTable_row.clear();
+    }
+    
+    std::ofstream InputTable;
+    InputTable.open("sample-LUT-in_for-one-input", std::ios::out);
+    for(size_t i=0; i<inputTable.size(); ++i){
+        for(size_t j=0; j<inputTable[0].size(); ++j){
+            InputTable << inputTable[i][j] << ' ';
+        }
+        InputTable << std::endl;
+    }
+    InputTable.close();
+    
+    std::cout << "end" << std::endl;
+
+    return 0;
+}
---
+++ tools/make_table/two-input/make_table.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <cstdio>
+#include <fstream>
+#include <chrono>
+#include <random>
+#include <vector>
+#include <thread>
+#include <future>
+#include <ctime>
+#include <ratio>
+#include <math.h>
+#include <cstddef>
+#include <iomanip>
+#include <string>
+
+#define NUM 4096
+
+int main(void)
+{
+  std::vector<std::vector<long>> inputT;
+  std::vector<long> inputT_row, outputT;
+
+  for (long i=0; i<2; ++i) {
+      for (long j=0; j<NUM; ++j) {
+          if (j<36) {
+              inputT_row.push_back(j+1);
+          } else {
+              inputT_row.push_back(100);
+          }
+      }
+      inputT.push_back(inputT_row);
+      inputT_row.clear();
+  }
+
+  long k=0, d=0;
+  for (long i=0; i<NUM; ++i) {
+      for(long j=0; j<NUM; ++j) {
+          ++d;
+          if (i<36 && j<36) {
+              outputT.push_back(inputT[0][i] + inputT[1][j]);
+              ++k;
+          } else {
+              outputT.push_back(1000);
+          }
+      }
+  }
+  std::cout<<"all output number(not100):"<<k<<",sum num:"<<d<<std::endl;
+  
+  std::ofstream InputTable;
+  InputTable.open("sample-LUT-in_for-two-input", std::ios::out);
+  for (size_t i=0; i<inputT.size(); ++i) {
+      for(size_t j=0; j<inputT[0].size(); ++j) {
+          InputTable << inputT[i][j] << ' ';
+      }
+      InputTable << std::endl;
+  }
+  InputTable.close();
+
+  std::ofstream OutputTable;
+  OutputTable.open("sample-LUT-out_for-two-input", std::ios::out);
+  for (size_t i=0; i<outputT.size(); ++i){
+      OutputTable << outputT[i] << ' ';
+  }
+  OutputTable << std::endl;
+  OutputTable.close();
+
+  std::cout << "end" << std::endl;
+  return 0;
+}
