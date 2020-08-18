8cf152d5dad1d741a2d430191094da0caee7abed
Hiromichi Iizuka <iizuka@eduam.co.jp>
2020-08-18 10:32:22 +0900

    tweak

--- test/test_one.sh
+++ test/test_one.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 while read row; do
-    x=`echo ${row} | cut -d , -f 1`
-    ex=`echo ${row} | cut -d , -f 2`
+    s1=`echo ${row} | fold -s1 | head -n1`
+    if [ ${s1} != '#' ]; then
+	x=`echo ${row} | cut -d , -f 1`
+	ex=`echo ${row} | cut -d , -f 2`
 
-    ./run_one.sh ${x} ${ex}
+	./run_one.sh ${x} ${ex}
+    fi
 done < test_one.csv
